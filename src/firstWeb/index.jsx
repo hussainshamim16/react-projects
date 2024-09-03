dcb3ce91603157a2681521713593832c6fe2f04c
hussainshamim16 <hj422748@gmail.com>
2024-09-03 10:18:51 -0700

    npm reset

---
+++ src/firstWeb/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Men = () => {
+  return (
+   <h1>men</h1>
+  )
+}
+
+export default Men
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Men from './firstWeb'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Men />
   </StrictMode>,
 )
